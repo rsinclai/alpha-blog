353726988722fa7782a7c3487f137c65486200d0
Roderick Sinclair <rsinclai@aol.com>
2020-08-30 16:29:14 -0700

    Message processing for new article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,19 @@ class ArticlesController < ApplicationController
   end
   
   def new
+	@article = Article.new # needed to avoid  error displaying messages on initial screen load
   end
  
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to article_path(@article)
+    #@article.save
+    #redirect_to article_path(@article)
+	if @article.save
+      flash[:notice] = "The article has been created successfully."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end  
 
 end
