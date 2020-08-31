8a745800ef40f95195007f0fdd6b876971fda4a6
Roderick Sinclair <rsinclai@aol.com>
2020-08-30 19:15:44 -0700

    Edit function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,16 +12,28 @@ class ArticlesController < ApplicationController
 	@article = Article.new # needed to avoid  error displaying messages on initial screen load
   end
  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    #@article.save
-    #redirect_to article_path(@article)
-	if @article.save
+    if @article.save
       flash[:notice] = "The article has been created successfully."
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "The article has been updated successfully."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
 end
