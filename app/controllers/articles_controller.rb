450be91e2c804857aa20ff46918acebeb57b67a6
Roderick Sinclair <rsinclai@aol.com>
2020-09-06 14:45:21 -0700

    DRY changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
   
   def index
@@ -13,13 +14,12 @@ class ArticlesController < ApplicationController
   end
  
   def edit
-    @article = Article.find(params[:id])
   end
   
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "The article has been created successfully."
+      flash_notice("The article has been created successfully.")
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,9 +27,8 @@ class ArticlesController < ApplicationController
   end  
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "The article has been updated successfully."
+    if @article.update(article_params)
+      flash_notice("The article has been updated successfully.")
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -37,9 +36,22 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+  
+    private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  
+  def flash_notice(flash_text)
+    flash[:notice] = flash_text
   end
 
 end
